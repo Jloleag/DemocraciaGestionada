2f5f605fc7bcc362ce4671d0fa45616c925e8ade
jose_ <jose_@Portatil-Jose>
2024-04-17 12:08:19 +0200

    He añadido mas democracia gestionada el miercoles

--- src/MalevelonCreek.java
+++ src/MalevelonCreek.java
@@ -6,6 +6,8 @@ public class MalevelonCreek {
 		public static void main(String[] args) {
 			
 			System.out.println("¡Por Supertierra!");
+			System.out.println("Toma una liber-taza");
+			System.out.println("Mi vida por Supertierra");
 
 		}
 
